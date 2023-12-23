31edbf4ff59e172bd8ecc50e92d9761424add2fc
CPTblackadder <rob@robvandyk.com>
2023-12-23 11:20:27 +0000

    part 2

--- src/day_23.rs
+++ src/day_23.rs
@@ -1,8 +1,17 @@
-use std::collections::VecDeque;
-
 use crate::{
     grid::{Coord, Direction, Grid},
     TaskCompleter,
+};
+use crossbeam::deque::{Steal, Worker};
+use rayon::spawn;
+use std::{
+    collections::VecDeque,
+    rc::Rc,
+    sync::{
+        atomic::{self, AtomicUsize},
+        mpsc, Arc, Mutex,
+    },
+    thread,
 };
 
 pub struct Task23;
@@ -54,7 +63,7 @@ fn get_longest_path_2(grid: &Grid<char>, start_tile: Coord, end_tile: Coord) -> 
         if tile == end_tile {
             if finishing_steps < s {
                 finishing_steps = s;
-                println!("Finished with {} steps", s);
+                // println!("Finished with {} steps", s);
             }
             continue;
         }
@@ -81,6 +90,79 @@ fn get_longest_path_2(grid: &Grid<char>, start_tile: Coord, end_tile: Coord) -> 
     finishing_steps
 }
 
+fn get_longest_path_multi_threading(grid: &Grid<char>, start_tile: Coord, end_tile: Coord) -> i64 {
+    let (s, r) = crossbeam::channel::unbounded();
+    let _ = s.send((
+        start_tile,
+        Grid::init_with_size(false, grid.width(), grid.height()),
+        0,
+    ));
+    let grid = Arc::new(grid.clone());
+    let threads_completed = Arc::new(Mutex::new(Box::new(0)));
+    let mut threads = vec![];
+    for _ in 0..8 {
+        let (send, rec) = (s.clone(), r.clone());
+        let mut finishing_steps: i64 = 0;
+        let grid = grid.clone();
+        let threads_completed = threads_completed.clone();
+        threads.push(thread::spawn(move || {
+            let mut thread_completed = false;
+            loop {
+                if let Ok((tile, mut steps, s)) = rec.try_recv() {
+                    if thread_completed {
+                        thread_completed = false;
+                        if let Ok(mut t) = threads_completed.lock() {
+                            *t.as_mut() -= 1;
+                        }
+                    }
+                    if tile == end_tile {
+                        if finishing_steps < s {
+                            finishing_steps = s;
+                            // println!("Finished with {} steps", s);
+                        }
+                        continue;
+                    }
+                    steps[tile] = true;
+                    let dirs = vec![
+                        Direction::Up,
+                        Direction::Down,
+                        Direction::Left,
+                        Direction::Right,
+                    ]
+                    .into_iter()
+                    .filter_map(|d| tile.translate(d, grid.as_ref()))
+                    .filter(|t| grid[*t] != '#' && !steps[*t])
+                    .collect::<Vec<Coord>>();
+                    if dirs.len() > 1 {
+                        for t in &dirs[1..] {
+                            let _ = send.send((*t, steps.clone(), s + 1));
+                        }
+                    }
+                    if dirs.len() > 0 {
+                        let _ = send.send((dirs[0], steps, s + 1));
+                    }
+                } else {
+                    if let Ok(mut t) = threads_completed.lock() {
+                        if !thread_completed {
+                            *t.as_mut() += 1;
+                        }
+                        if t.as_ref() == &8 {
+                            return finishing_steps;
+                        }
+                    }
+                    thread_completed = true;
+                }
+            }
+        }));
+    }
+
+    threads
+        .into_iter()
+        .map(|x| x.join().unwrap())
+        .max()
+        .unwrap()
+}
+
 impl TaskCompleter for Task23 {
     fn do_task_1(&self) -> String {
         let contents: &str = include_str!("../input/day_23/input");
@@ -103,7 +185,7 @@ impl TaskCompleter for Task23 {
 
     fn do_task_2(&self) -> String {
         let contents: &str = include_str!("../input/day_23/input");
-        let g = Grid::from_string(contents, false);
+        let g: Grid<char> = Grid::from_string(contents, false);
         let start_tile = (0..g.width() as i64)
             .into_iter()
             .find(|x| g[Coord::new(*x, 0)] == '.')
@@ -112,7 +194,7 @@ impl TaskCompleter for Task23 {
             .into_iter()
             .find(|x| g[Coord::new(*x, g.height() as i64 - 1)] == '.')
             .unwrap();
-        get_longest_path_2(
+        get_longest_path_multi_threading(
             &g,
             Coord::new(start_tile, 0),
             Coord::new(end_tile, g.height() as i64 - 1),
